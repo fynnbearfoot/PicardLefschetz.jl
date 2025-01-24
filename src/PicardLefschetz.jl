68b692fbd7b93d005e586b2ecaf7ea30fe32166c
Anne Weber <anne.weber@mailbox.org>
2025-01-24 10:43:29 +0000

    Initial commit

--- src/PicardLefschetz.jl
+++ src/PicardLefschetz.jl
@@ -1,5 +1,19 @@
 module PicardLefschetz
 
+using LinearAlgebra
+
 # Write your package code here.
+function hello() 
+	println("hello, World")
+end
+
+"""
+    square(x) 
+
+Returns the square of `x`.
+"""
+square(x) = x^2
+
+export square
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "PicardLefschetz.jl" begin
     # Write your tests here.
+
+    @test square(3) == 9
+    @test square(π) ≈ 9.8696 rtol=1e-4
+
 end
